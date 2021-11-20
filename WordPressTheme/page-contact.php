2d4c8a257b9ba10fb1d877db17018835117be93c
bifree <bifree2bfree@gmail.com>
2021-11-21 02:10:24 +0900

    ok

--- WordPressTheme/page-contact.php
+++ WordPressTheme/page-contact.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
 
-<?php the_content(); ?>
+<div class="contact-main">
+	<div class="gray-bg">
+		<div class="container">
+			<h1>整体院 神威　患者様入力フォーム（お問い合わせ）</h1>
+			<p>整体院 神威は完全予約制になっておりますので、事前にご予約を頂く必要がありますのでご注意ください。</p>
+			<p>診察のご予約はお電話、もしくはメールフォームからお願いします。 ご不明な点や質問などもお気軽にお問い合わせ下さい。</p>
+			<?php the_content(); ?>
+		</div>
+	</div>
+</div>
+
 
 <?php get_footer(); ?>
