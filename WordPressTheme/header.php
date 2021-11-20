c4dea32d371c868dd70dd2e316535d8b74179ab4
bifree <bifree2bfree@gmail.com>
2021-11-21 02:20:26 +0900

    responsive

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -1,105 +1,116 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-  <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Document</title>
+		<title>市川市で随一の【膝痛専門】整体院 神威 本八幡駅徒歩1分</title>
 		<link rel="stylesheet" href="assets/css/style.css">
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style2.css">
 		<link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
 		<link href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" rel="stylesheet">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico">
 		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-  <?php wp_body_open(); ?>
+		<meta property="og:site_name" content="市川市で随一の【膝痛専門】整体院 神威 本八幡駅徒歩1分" />
+		<meta name="description"  content="千葉県市川市で膝痛でお困りの方へ。どこにいっても治らなかった慢性膝痛を根本から取り除く施術をあなたも体験しませんか？あなたの痛みのお悩みに真剣に向き合います" />
+		<meta property="og:title" content="市川市で唯一の【腰痛専門】整体院　神威　本八幡駅徒歩１分" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="https://ichikawashi-seitai.com/" />
+		<meta property="og:image" content="https://ichikawashi-seitai.com/wp/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" />
+		<meta property="og:site_name" content="市川市で唯一の【腰痛専門】整体院　神威　本八幡駅徒歩１分" />
+		<meta property="og:description" content="千葉県市川市で慢性腰痛・ヘルニア・坐骨神経痛・脊柱管狭窄症・ギックリ腰・腰椎すべり症などの腰痛でお困りの方へ。どこにいっても治らなかった腰痛を根本から取り除く施術をあなたも体験しませんか？あなたの痛みのお悩みに真剣に向き合います。" />
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:title" content="市川市で唯一の【腰痛専門】整体院　神威　本八幡駅徒歩１分" />
+		<meta name="twitter:description" content="千葉県市川市で慢性腰痛・ヘルニア・坐骨神経痛・脊柱管狭窄症・ギックリ腰・腰椎すべり症などの腰痛でお困りの方へ。どこにいっても治らなかった腰痛を根本から取り除く施術をあなたも体験しませんか？あなたの痛みのお悩みに真剣に向き合います。" />
+		<meta name="twitter:image" content="https://ichikawashi-seitai.com/wp/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" />
+		<meta itemprop="image" content="https://ichikawashi-seitai.com/wp/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" />
+
+	</head>
+
+	<body>
 		<header>
-      <div class="l-header">
-        <div class="header-top ">
-          <div class="header-top__left">
-            <a class="left__logo" href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png"></a>
-          </div>
+			<div class="header-top ">
+				<div class="header-top__left">
+					<a class="left__logo" href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png"></a>
+				</div>
 
-          <div class="header-top__right">
-            <div class="right__tel">
-              <a class="right__tel__btn" href="tel:090-4607-3624">
-                <p ><i class="fas fa-phone-square-alt"></i></p>
-                <div class="right__tel__text">
-                  <p class="right__tel__time">診察予約はお電話：9:00-22:00</p>
-                  <p class="right__tel__number">090-4607-362</p>
-                </div>	
-              </a>
-            </div>
-            <div class="right__mail">
-              <a class="right__mail__btn" href=""><i class="far fa-envelope"></i>メールでのご予約</a>
-            </div>
-          </div>
-        </div>
-          
-        <div class="pc-menu">
-          <ul class="pc-menu__list">
-            <li class="pc-menu__list__item"><a href="#"></span>トップページ</a></li>
-            <li class="pc-menu__list__item"><a href="#fascia">Fascia療法とは</a></li>
-            <li class="pc-menu__list__item"><a href="#voice">お客様の声</a></li>
-            <li class="pc-menu__list__item"><a href="#profile-thought">院長プロフィール</a></li>
-            <li class="pc-menu__list__item"><a href="#select">選ばれる理由</a></li>
-            <li class="pc-menu__list__item"><a href="#treatment">施術の流れ</a></li>
-            <li class="pc-menu__list__item"><a href="#suitable">こんな方はNG</a></li>
-          </ul>
-        </div>
+				<div class="header-top__right">
+					<div class="right__tel">
+						<a class="right__tel__btn" href="tel:090-4607-3624">
+							<p ><i class="fas fa-phone-square-alt"></i></p>
+							<div class="right__tel__text">
+								<p class="right__tel__time">診察予約はお電話：9:00-22:00</p>
+								<p class="right__tel__number">090-4607-362</p>
+							</div>	
+						</a>
+					</div>
+					<div class="right__mail">
+						<a class="right__mail__btn" href="<?php echo home_url('/contact'); ?>"><i class="far fa-envelope"></i>メールでのご予約</a>
+					</div>
+				</div>
+			</div>
+				
+			<div class="pc-menu">
+				<ul class="pc-menu__list">
+					<li class="pc-menu__list__item"><a href="#"></span>トップページ</a></li>
+					<li class="pc-menu__list__item"><a href="#fascia">Fascia療法とは</a></li>
+					<li class="pc-menu__list__item"><a href="#voice">お客様の声</a></li>
+					<li class="pc-menu__list__item"><a href="#profile-thought">院長プロフィール</a></li>
+					<li class="pc-menu__list__item"><a href="#select">選ばれる理由</a></li>
+					<li class="pc-menu__list__item"><a href="#treatment">施術の流れ</a></li>
+					<li class="pc-menu__list__item"><a href="#suitable">こんな方はNG</a></li>
+				</ul>
+			</div>
 
-        <div id="open" class="sp-menu-btn">
-          <span class="menu-btn__text"><i class="fas fa-bars open"></i><br>menu</span>
-        </div> 
-        
-        <div id="close" class="hide sp-menu-btn">
-          <span class="menu-btn__text"><i class="fas fa-times close"></i><br>close</span>
-        </div>
+			<div id="open" class="sp-menu-btn">
+				<span class="menu-btn__text"><i class="fas fa-bars open"></i><br>menu</span>
+			</div> 
+			
+			<div id="close" class="hide sp-menu-btn">
+				<span class="menu-btn__text"><i class="fas fa-times close"></i><br>close</span>
+			</div>
 
-        <div class="sp-menu">
-          <ul class="sp-menu__list">
-            <li class="sp-menu__list__item">
-              <a href="#">
-                <p>トップページ</p>
-                <p><i class="fas fa-arrow-circle-right"></i></p>
-              </a>
-            </li>
-            <li class="sp-menu__list__item">
-              <a href="#fascia">
-                <p>Fascia療法とは</p>
-                <p><i class="fas fa-arrow-circle-right"></i></p>
-              </a>
-            </li>
-            <li class="sp-menu__list__item">
-              <a href="#voice">
-                <p>お客様の声</p>
-                <p><i class="fas fa-arrow-circle-right"></i></p>
-              </a></li>
-            <li class="sp-menu__list__item">
-              <a href="#profile-thought">
-                <p>院長プロフィール</p>
-                <p><i class="fas fa-arrow-circle-right"></i></p>
-              </a>
-            </li>
-            <li class="sp-menu__list__item">
-              <a href="#select">
-                <p>選ばれる理由</p>
-                <p><i class="fas fa-arrow-circle-right"></i></p>
-              </a></li>
-            <li class="sp-menu__list__item">
-              <a href="#treatment">
-                <p>施術の流れ</p>
-                <p><i class="fas fa-arrow-circle-right"></i></p>
-              </a>
-            </li>
-            <li class="sp-menu__list__item">
-              <a href="#suitable">
-                <p>こんな方はNG</p>
-                <p><i class="fas fa-arrow-circle-right"></i></p>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
+			<div class="sp-menu">
+				<ul class="sp-menu__list">
+					<li class="sp-menu__list__item">
+						<a href="#">
+							<p>トップページ</p>
+							<p><i class="fas fa-arrow-circle-right"></i></p>
+						</a>
+					</li>
+					<li class="sp-menu__list__item">
+						<a href="#fascia">
+							<p>Fascia療法とは</p>
+							<p><i class="fas fa-arrow-circle-right"></i></p>
+						</a>
+					</li>
+					<li class="sp-menu__list__item">
+						<a href="#voice">
+							<p>お客様の声</p>
+							<p><i class="fas fa-arrow-circle-right"></i></p>
+						</a></li>
+					<li class="sp-menu__list__item">
+						<a href="#profile-thought">
+							<p>院長プロフィール</p>
+							<p><i class="fas fa-arrow-circle-right"></i></p>
+						</a>
+					</li>
+					<li class="sp-menu__list__item">
+						<a href="#select">
+							<p>選ばれる理由</p>
+							<p><i class="fas fa-arrow-circle-right"></i></p>
+						</a></li>
+					<li class="sp-menu__list__item">
+						<a href="#treatment">
+							<p>施術の流れ</p>
+							<p><i class="fas fa-arrow-circle-right"></i></p>
+						</a>
+					</li>
+					<li class="sp-menu__list__item">
+						<a href="#suitable">
+							<p>こんな方はNG</p>
+							<p><i class="fas fa-arrow-circle-right"></i></p>
+						</a>
+					</li>
+				</ul>
+			</div>
 		</header>
