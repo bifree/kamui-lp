89c1dfa589b21fcff8722cf6f3af8994948e2328
bifree <bifree2bfree@gmail.com>
2021-11-20 13:56:40 +0900

    header編集

--- WordPressTheme/front-page.php
+++ WordPressTheme/front-page.php
@@ -8,8 +8,21 @@
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style2.css">
 		<link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
 		<link href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" rel="stylesheet">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico">
 		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
 		<meta property="og:site_name" content="市川市で随一の【膝痛専門】整体院 神威 本八幡駅徒歩1分" />
+		<meta name="description"  content="千葉県市川市で膝痛でお困りの方へ。どこにいっても治らなかった慢性膝痛を根本から取り除く施術をあなたも体験しませんか？あなたの痛みのお悩みに真剣に向き合います" />
+		<meta property="og:title" content="市川市で唯一の【腰痛専門】整体院　神威　本八幡駅徒歩１分" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="https://ichikawashi-seitai.com/" />
+		<meta property="og:image" content="https://ichikawashi-seitai.com/wp/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" />
+		<meta property="og:site_name" content="市川市で唯一の【腰痛専門】整体院　神威　本八幡駅徒歩１分" />
+		<meta property="og:description" content="千葉県市川市で慢性腰痛・ヘルニア・坐骨神経痛・脊柱管狭窄症・ギックリ腰・腰椎すべり症などの腰痛でお困りの方へ。どこにいっても治らなかった腰痛を根本から取り除く施術をあなたも体験しませんか？あなたの痛みのお悩みに真剣に向き合います。" />
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:title" content="市川市で唯一の【腰痛専門】整体院　神威　本八幡駅徒歩１分" />
+		<meta name="twitter:description" content="千葉県市川市で慢性腰痛・ヘルニア・坐骨神経痛・脊柱管狭窄症・ギックリ腰・腰椎すべり症などの腰痛でお困りの方へ。どこにいっても治らなかった腰痛を根本から取り除く施術をあなたも体験しませんか？あなたの痛みのお悩みに真剣に向き合います。" />
+		<meta name="twitter:image" content="https://ichikawashi-seitai.com/wp/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" />
+		<meta itemprop="image" content="https://ichikawashi-seitai.com/wp/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" />
 
 	</head>
 
@@ -31,7 +44,7 @@
 						</a>
 					</div>
 					<div class="right__mail">
-						<a class="right__mail__btn" href=""><i class="far fa-envelope"></i>メールでのご予約</a>
+						<a class="right__mail__btn" href="<?php echo home_url('/contact'); ?>"><i class="far fa-envelope"></i>メールでのご予約</a>
 					</div>
 				</div>
 			</div>
